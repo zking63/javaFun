39909824876d68c1f39f9c43803b78cd2241ced7
zking63 <zoemking63@gmail.com>
2021-02-08 10:22:43 -0500

    first commit

---
+++ firstProgram/Name.java
@@ -0,0 +1,7 @@
+public class Name {
+    public static void main(String[] args) {
+        System.out.println("My name is Zoe King");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Indianapolis, IN");
+    }
+}
